333e9de69da90ca2b9bf7191362d981b8c1016ac
Ignacio Pedrosa <ignapedrosa2@gmail.com>
2023-06-20 19:39:27 -0300

    Agregamos estilos

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,18 @@
 // API GIF
 // l6QY9LzoniKoGtmEJvU5EmCPZLpPNfPI 
 
+
+
 export const GifExpertApp = () => {
   return (
     <>
+    {/* Titulo */}
         <h1>GifExpertApp</h1>
+
+      {/* Input */}
+
+      {/* Listado de Gif */}
+        {/* Gif Item */}
     </>
   )
 }
